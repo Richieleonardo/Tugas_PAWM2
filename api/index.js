b2a7ead3b6347cb6991b2ce9c7a8ba31e2eaea50
Richie Leonardo <richie.leonardoo@gmail.com>
2024-11-14 22:50:54 +0700

    add API from old branch in repo Tugas_PAWM1

--- api/index.js
+++ api/index.js
@@ -4,11 +4,14 @@ const app = express();
 
 //MIDDLEWARE
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, '../styles')));
 
 //ROUTES
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
+
+const UserRouter = require('./user');
+app.use('/user', UserRouter);
 
 module.exports = app;
