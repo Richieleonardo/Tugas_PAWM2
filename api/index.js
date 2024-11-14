64b05f287479d763015bd479c13bbec6f4673c62
Richie Leonardo <richie.leonardoo@gmail.com>
2024-11-14 22:39:15 +0700

    index.html

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 app.use(express.json());
-app.get("/", (req, res) => res.send("Test push"));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
