eaa0f3e4bb8c4e1615eab4d3f93b65c892c2b06a
Richie Leonardo <richie.leonardoo@gmail.com>
2024-11-14 23:13:12 +0700

    hehe lupa masukin database ke index

--- api/index.js
+++ api/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const dotenv = require('dotenv').config({path: '.env'});
 
+require('./database/db');
 //MIDDLEWARE
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../styles')));
 
 //ROUTES
-app.listen(3000, () => console.log("Server ready on port 3000."));
+
+app.listen(process.env.PORT, () => console.log("Server ready on port 3001."));
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 
