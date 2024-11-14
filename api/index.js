11ff42655c266d4029f87ee3dd0e985ac3d1b7b8
Richie Leonardo <richie.leonardoo@gmail.com>
2024-11-14 22:43:56 +0700

    test change

--- api/index.js
+++ api/index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+//MIDDLEWARE
 app.use(express.json());
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 
+
+//ROUTES
 app.listen(3000, () => console.log("Server ready on port 3000."));
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 
 module.exports = app;
